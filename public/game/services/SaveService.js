c259fa2955cc51140c0b5f10ecff0865f8476c3c
nathangraff-create <nathangraff@hotmail.com>
2026-03-24 15:43:04 -0300

    add styles

--- public/game/services/SaveService.js
+++ public/game/services/SaveService.js
@@ -1,7 +1,7 @@
 export default class SaveService {
   async load() {
     try {
-      const res = await fetch("/player/1"); // ou sua API real futura
+      const res = await fetch("/player/1");
       return await res.json();
     } catch (e) {
       console.warn("API falhou, usando localStorage");
